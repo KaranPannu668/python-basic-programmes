41ded1d1819b86980addf045368ed67bb30e14a1
KaranPannu668 <karanpannu668@gmail.com>
2022-07-25 19:11:13 +0530

    Circles using tkinter

--- functions/morefunct.py
+++ functions/morefunct.py
@@ -1,17 +1,26 @@
-from operator import xor
-from textwrap import fill
-from turtle import back
-
-
+import math
 try:
     import tkinter
 except ImportError:  # python 2
     import Tkinter as tkinter
 
 
-def parabola(x):
-    y = x*x / 100
-    return y
+def parabola(page, size):
+    for x in range(size):
+        y = x*x / size
+        plot(page, x, y)
+        plot(page, -x, y)
+
+# Modify the circle function so that it allows the color of the circle to be specified and defaults to red  if a color is not given.
+def circle(page, radius, g, h, color='red'):
+    page.create_oval(g+radius, h+radius, g-radius, h- radius, outline = color, width=2)
+    # for x in range(g*100, (g+radius)*100):
+    #     x /= 100
+    #     y = h + (math.sqrt(radius **2 - ((x-g) ** 2)))
+    #     plot(page, x, y)
+    #     plot(page, x, 2*h-y)
+    #     plot(page, 2* g -x, y)
+    #     plot(page, 2*g-x, 2*h-y)
 
 
 def draw_axes(page):
@@ -24,26 +33,23 @@ def draw_axes(page):
     print(locals())
 
 
-def plot(canvas, x, y):
-    canvas.create_line(x, y, x+1, y+1, fill='red')
+def plot(page, x, y):
+    page.create_line(x, -y, x+1, -y+1, fill='red')
 
 
 mainWindow = tkinter.Tk()
+
 mainWindow.title('Parabola')
 mainWindow.geometry('640x480+10+10')
 
-canvas = tkinter.Canvas(mainWindow, width=320, height=480)
+canvas = tkinter.Canvas(mainWindow, width=640, height=480)
 canvas.grid(row=0, column=0)
 
-canvas2 = tkinter.Canvas(mainWindow, width=320, height=480, background='blue')
-canvas2.grid(row=0, column=1)
-
-print(repr(canvas), repr(canvas2))
 draw_axes(canvas)
-draw_axes(canvas2)
 
-for x in range(-100, 100):
-    y = parabola(x)
-    plot(canvas, x, -y)
+parabola(canvas, 100)
+parabola(canvas, 200)
+circle(canvas, 100, 100, 100)
+circle(canvas, 100, 100, -100, color='green')
 
 mainWindow.mainloop()

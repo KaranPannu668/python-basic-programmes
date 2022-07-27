b981a2e744b65c2afb92830cd46c1daecaae8fba
KaranPannu668 <karanpannu668@gmail.com>
2022-07-27 10:39:20 +0530

    BlackJack complete

---
+++ BlackJack/import_test.py
@@ -0,0 +1,10 @@
+# import blackjack
+
+# blackjack._deal_card(blackjack.dealer_card_frame)
+# blackjack.play()
+
+
+personal_details = ('Karan', 21, 'India')
+
+name, _, country = personal_details
+print(name, country)

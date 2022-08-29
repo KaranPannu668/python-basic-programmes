237b9cb4d4bdd456fe198c9af979ce04c4762043
KaranPannu668 <karanpannu668@gmail.com>
2022-08-29 17:03:27 +0530

    Jukebox continued

--- MusicBrowser/jukebox.py
+++ MusicBrowser/jukebox.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 import sqlite3
 from webbrowser import BackgroundBrowser
 try:
@@ -21,15 +22,66 @@ class Scrollbox(tkinter.Listbox):
         self['yscrollcommand'] = self.scrollbar.set
 
 
-def get_albums(event):
+class DataListBox(Scrollbox):
+
+    def __init__(self, window, connection, table, field, sort_order=(), **kwargs):
+        super().__init__(window, **kwargs)
+
+        self.cursor = connection.cursor()
+        self.table = table
+        self.field = field
+
+        self.sql_select = "SELECT "+ self.field + ", _id" + " FROM " + self.table
+        if sort_order:
+            self.sql_sort = " ORDER BY " + ','.join(sort_order)
+        else:
+            self.sql_sort = " ORDER BY " + self.field
+    
+    def clear(self):
+        self.delete(0,tkinter.END)
+    
+    def requery(self, link_value=None):
+        if link_value:
+            sql = self.sql_select + " WHERE " + "artist" + "=?" + self.sql_sort
+            print(sql)  # TODO delete this line
+            self.cursor.execute(sql, (link_value,))
+        else:
+            print(self.sql_select + self.sql_sort)  # TODO delete this line
+            self.cursor.execute(self.sql_select + self.sql_sort)
+
+        # clear the listbox contents before re-loading
+        self.clear()
+        for value in self.cursor:
+            self.insert(tkinter.END, value[0])
+
+    def on_select(self, event):
+        print(self is event.widget)
+        index = self.curselection()[0]
+        value = self.get(index),
+
+        # get the artist ID from the database row
+        link_id = self.cursor.execute(self.sql_select + " WHERE " + self.field + "=?" , value).fetchone()[1]
+        albumList.requery(link_id)
+
+        # artist_id = conn.execute("SELECT artists._id FROM artists WHERE artists.name=?", artist_name).fetchone()
+        # alist = []
+        # for row in conn.execute("SELECT albums.name FROM albums WHERE albums.artist=? ORDER BY albums.name", artist_id):
+        #     alist.append(row[0])
+        # albumLV.set(tuple(alist))
+        # songLV.set(("Choose an album",))
+
+
+def get_songs(event):
     lb = event.widget
-    index = lb.curselection()[0]
-    artist_name = lb.get(index),
-    artist_id = conn.execute("SELECT artists._id FROM artists WHERE artists.name=?", artist_name).fetchone()
+    index = int(lb.curselection()[0])
+    album_name = lb.get(index),
+
+    # get the artist ID from the database row
+    album_id = conn.execute("SELECT albums._id FROM albums WHERE albums.name=?", album_name).fetchone()
     alist = []
-    for row in conn.execute("SELECT albums.name FROM albums WHERE albums.artist=? ORDER BY albums.name", artist_id):
-        alist.append(row[0])
-    albumLV.set(tuple(alist))
+    for x in conn.execute("SELECT songs.title FROM songs WHERE songs.album=? ORDER BY songs.track", album_id):
+        alist.append(x[0])
+    songLV.set(tuple(alist))
 
 
 mainWindow = tkinter.Tk()
@@ -52,26 +104,28 @@ tkinter.Label(mainWindow, text='Albums').grid(row=0, column=1)
 tkinter.Label(mainWindow, text='Songs').grid(row=0, column=2)
 
 # =====Artists Listbox======
-artistList = Scrollbox(mainWindow)
+artistList = DataListBox(mainWindow, conn, "artists", "name")
 artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
 artistList.config(border=2, relief='sunken')
 
-for artist in conn.execute("SELECT artists.name FROM artists ORDER BY artists.name"):
-    artistList.insert(tkinter.END, artist[0])
- 
-    artistList.bind('<<ListboxSelect>>', get_albums)
+artistList.requery()
+artistList.bind('<<ListboxSelect>>', get_albums)
 
 # ======Albums Listbox=======
 albumLV = tkinter.Variable(mainWindow)
 albumLV.set(("Choose an artist",))
-albumList = tkinter.Listbox(mainWindow, listvariable=albumLV)
+albumList = DataListBox(mainWindow, conn, "albums", "name", sort_order=("name",))
+albumList.requery(12)
 albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
 albumList.config(border=2, relief='sunken')
+
+albumList.bind('<<ListboxSelect>>', get_songs)
 
 # ==========Songs Listbox============
 songLV = tkinter.Variable(mainWindow)
 songLV.set(("Choose an album",))
-songList = tkinter.Listbox(mainWindow, listvariable=songLV)
+songList = DataListBox(mainWindow, conn, "songs", "title", ("track", "title"))
+songList.requery()
 songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
 songList.config(border=2, relief='sunken')
 

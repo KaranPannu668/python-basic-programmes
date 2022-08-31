b2d08b2a517fff8b4d08d00068b1e1bb100b16f9
KaranPannu668 <karanpannu668@gmail.com>
2022-08-31 16:41:23 +0530

    Generators

--- genexample/size.py
+++ genexample/size.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def my_range(n: int):
     print("my_range starts")
     start = 0
@@ -8,22 +9,24 @@ def my_range(n: int):
         yield start
         start += 1
 
-
-_ = input("link 12") 
-# big_range = range(10000)
-big_range = my_range(5)
-_ = input("link 15") 
+big_range = range(5)
+# big_range = my_range(5)
+# _ = input("link 14") 
 
 print("big_range is {} bytes".format(sys.getsizeof(big_range)))
 
 # create a list containing all the values in big_range
 big_list = []
 
-_=input("line 22")
+# _=input("line 22")
 for val in big_range:
-    _=input("line 24 - inside loop")
+    # _=input("line 24 - inside loop")
     big_list.append(val)
 
 print("big_list is {} bytes".format(sys.getsizeof(big_list)))
 print(big_range)
 print(big_list)
+
+print("looping again ... or not")
+for i in big_range:
+    print("i is {}".format(i))

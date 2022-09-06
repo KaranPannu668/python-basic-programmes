9d05c2d5f8c5e0ef73e38e3fccf6829adbdda981
KaranPannu668 <karanpannu668@gmail.com>
2022-09-06 20:08:12 +0530

    Big O begins

---
+++ Big_O/bubble_sort.py
@@ -0,0 +1,17 @@
+def bubble_sort(data: list) -> None:
+    """Sorts a list in place."""
+    n = len(data)
+    comparison_count = 1
+
+    for i in range(n-1):
+        for j in range(n-1):
+            comparison_count += 1
+            if data[j] > data[j+1]:
+                data[j], data[j+1] = data[j+1], data[j]
+        
+        print(f"End of pass {i}. `data` is now {data}")
+    print(f"comparison_count is {comparison_count}")
+
+
+numbers = [3,4,6,5,23,2,5,7]
+bubble_sort(numbers)

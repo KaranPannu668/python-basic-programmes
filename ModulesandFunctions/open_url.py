127fcbd8693b17ce67c1e12c2dd84a90cfe19e89
KaranPannu668 <karanpannu668@gmail.com>
2022-09-08 23:05:00 +0530

    datetime revisited

---
+++ ModulesandFunctions/open_url.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# webbrowser.open('https://samsung.com')
+
+# chrome = webbrowser.get(using='google-chrome')
+chrome = webbrowser.get('C:/Program Files/Google/Chrome/Application/chrome.exe %s')
+chrome.open('https://www.apple.com')
